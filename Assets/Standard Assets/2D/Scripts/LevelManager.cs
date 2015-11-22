9f912dcc28886842d48f40102f4366a334df2258
zhenga94 <zhenga@0587335598.wireless.umich.net>
2015-11-21 23:51:57 -0500

    stuff from Andrew
    all the pngs that i created
    parallaxing script
    pauseMenu
    levelmanager
    main menu
    and my test

---
+++ Assets/Standard Assets/2D/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadScene(){
+		Application.LoadLevel (1);
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
